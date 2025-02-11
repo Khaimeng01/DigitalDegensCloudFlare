4e296914167d5b4d8d7a6772e2f3abaa7bba20d3
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-11 15:21:44 +0800

    Commit 6.1 : Cloudflare changes 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/Marketing-Website/',
-  plugins: [react()],
-})
+    base: '/', // Change this from '/Marketing-Website/' to '/'
+    plugins: [react()],
+});
