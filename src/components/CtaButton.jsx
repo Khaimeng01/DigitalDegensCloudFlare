fa96f15921a506eaf3c46ecf6f07107f96fc5383
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-11 14:54:50 +0800

    Commit 2 : Button Design

--- src/components/CtaButton.jsx
+++ src/components/CtaButton.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const CtaButton = ({name}) => {
-  return (
-    <button className="bg-orange-600 hover:bg-orange-500 hover:shadow hover:shadow-orange-500 text-lg text-slate-50 rounded-3xl px-4 py-2 w-fit transition-all duration-300">
-      {name}
-    </button>
-  );
+const CtaButton = ({ name }) => {
+    return (
+        // <button className="bg-orange-600 hover:bg-orange-500 hover:shadow hover:shadow-orange-500 text-lg text-slate-50 rounded-3xl px-4 py-2 w-fit transition-all duration-300">
+        //   {name}
+        // </button>
+
+        <button className="bg-blue-400 hover:bg-blue-300 hover:shadow hover:shadow-blue-300 text-white rounded-3xl px-4 py-2 w-[153px] text-lg transition-all duration-300">
+            {name}
+        </button>
+
+    );
 };
 
 export default CtaButton;
