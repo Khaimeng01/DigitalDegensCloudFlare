bffc32856dc1492fdc36c9fed42a3be91228bf04
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-12 12:23:35 +0800

    no message

--- utils/Routings.jsx
+++ utils/Routings.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-// import { Redirect } from "react-router-dom";
 import Home from "../src/components/Home";
 import AppDevelopment from "../src/services/AppDevelopment";
 import WebDevelopment from "../src/services/WebDevelopment";
@@ -19,29 +18,33 @@ import book3 from "../src/img/portfolio/books-3.jpg";
 import Hireus from "../src/services/Hireus";
 
 const Routings = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/Marketing-Website/" element={<Home />} />
-        <Route path="/app-development" element={<AppDevelopment />} />
-        <Route path="/web-development" element={<WebDevelopment />} />
-        <Route path="/seo-optimization" element={<Seo />} />
-        <Route path="/campagin-creation" element={<Campaign />} />
-        <Route path="/hireus" element={<Hireus />} />
-        <Route path="Marketing-Website/portfolio">
-          <Route path="app1" element={<ProductsTemp img={app1} />} />
-          <Route path="product1" element={<ProductsTemp img={product1} />} />
-          <Route path="brand1" element={<ProductsTemp img={branding1} />} />
-          <Route path="book1" element={<ProductsTemp img={book1} />} />
-          <Route path="app2" element={<ProductsTemp img={app2} />} />
-          <Route path="book3" element={<ProductsTemp img={book3} />} />
-          <Route path="product3" element={<ProductsTemp img={product3} />} />
-          <Route path="book2" element={<ProductsTemp img={book2} />} />
-          <Route path="app3" element={<ProductsTemp img={app3} />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                {/* Home route now matches "/" */}
+                <Route path="/" element={<Home />} />
+
+                <Route path="/app-development" element={<AppDevelopment />} />
+                <Route path="/web-development" element={<WebDevelopment />} />
+                <Route path="/seo-optimization" element={<Seo />} />
+                <Route path="/campaign-creation" element={<Campaign />} />
+                <Route path="/hireus" element={<Hireus />} />
+
+                {/* Portfolio Nested Routes */}
+                <Route path="/portfolio">
+                    <Route path="app1" element={<ProductsTemp img={app1} />} />
+                    <Route path="product1" element={<ProductsTemp img={product1} />} />
+                    <Route path="brand1" element={<ProductsTemp img={branding1} />} />
+                    <Route path="book1" element={<ProductsTemp img={book1} />} />
+                    <Route path="app2" element={<ProductsTemp img={app2} />} />
+                    <Route path="book3" element={<ProductsTemp img={book3} />} />
+                    <Route path="product3" element={<ProductsTemp img={product3} />} />
+                    <Route path="book2" element={<ProductsTemp img={book2} />} />
+                    <Route path="app3" element={<ProductsTemp img={app3} />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 };
 
 export default Routings;
