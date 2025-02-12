78cf6bc3a929d4e0a3e911f0f22b13f3b6224969
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-12 12:18:03 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-////import { defineConfig } from 'vite';
-////import react from '@vitejs/plugin-react';
-
-////export default defineConfig({
-////    base: './', // Change '/' to './' for Cloudflare Pages
-////    plugins: [react()],
-////});
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/Marketing-Website/', 
+    base: './', // Change '/' to './' for Cloudflare Pages
     plugins: [react()],
 });
+
+////import { defineConfig } from 'vite';
+////import react from '@vitejs/plugin-react';
+
+////export default defineConfig({
+////    base: '/Marketing-Website/', 
+////    plugins: [react()],
+////});
 
