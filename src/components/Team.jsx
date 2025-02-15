c0b0b16cbbc0880a4daf2acf89bfc59e499c469b
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-15 21:41:29 +0800

    no message

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect } from "react";
-import team1 from "../img/team/team-1.jpg";
 import team2 from "../img/team/team-2.jpg";
-import team3 from "../img/team/team-3.jpg";
-import team4 from "../img/team/team-4.jpg";
+import team5 from "../img/team/team-3.jpg";
+import Bal from "../img/team/Balreen.jpeg";
+import Tash from "../img/team/Tash.jpeg";
+import Surjit from "../img/team/Surjit.jpeg";
+import Syarifah from "../img/team/Syarifah.jpeg";
+import Shazlin from "../img/team/Shazlin.jpeg";
+import Raj from "../img/team/Raj.jpeg";
+
+
+
+//import team5 from "../img/team/Tashreen.jpg";
+
+
 // aos
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -38,9 +48,9 @@ const Team = () => {
 
 
             <div className="flex flex-col justify-center xs:flex-row gap-6 p-5 lg:p-0 items-center">
-                <TeamImage image={team1} name={"Tashreen Sidhu"} role={"Partner"} />
-                <TeamImage image={team2} name={"Balreen Sidhu"} role={"Partner"} />
-                <TeamImage image={team3} name={"Surjit Sidhu"} role={"Partner"} />
+                <TeamImage image={Tash} name={"Tashreen Sidhu"} role={"Partner"} />
+                <TeamImage image={Bal} name={"Balreen Sidhu"} role={"Partner"} />
+                <TeamImage image={Surjit} name={"Surjit Sidhu"} role={"Partner"} />
 
 
             </div>
@@ -48,9 +58,9 @@ const Team = () => {
             <div className="pb-5"></div>
 
             <div className="flex flex-col justify-center xs:flex-row gap-6 p-5 lg:p-0 items-center">
-                <TeamImage image={team1} name={"Prajjwal Raj Shrestha"} role={"Photography Director"} />
-                <TeamImage image={team2} name={"Nurshaiza Shazlin"} role={"Associate"} />
-                <TeamImage image={team3} name={"Syarifah Nurnajirah"} role={"Associate"} />
+                <TeamImage image={Raj} name={"Prajjwal Raj Shrestha"} role={"Photography Director"} />
+                <TeamImage image={Shazlin} name={"Nurshaiza Shazlin"} role={"Associate"} />
+                <TeamImage image={Syarifah} name={"Syarifah Nurnajirah"} role={"Associate"} />
 
 
             </div>
