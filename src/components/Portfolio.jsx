a45679c262bfb51cfead18993a4d363ebca488ed
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-23 12:09:13 +0800

    no message

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,6 +8,14 @@ import branding1 from "../img/portfolio/branding-1.jpg";
 import book1 from "../img/portfolio/books-1.jpg";
 import book2 from "../img/portfolio/books-2.jpg";
 import book3 from "../img/portfolio/books-3.jpg";
+import photoIcon from "../img/portfolio/Photography.png";
+import SocialIcon from "../img/portfolio/Social Media Creatives.png";
+import VideosIcon from "../img/portfolio/Videos.png";
+import photoShootIcon from "../img/portfolio/photoshoot.jpg";
+
+
+
+
 
 // aos
 import AOS from "aos";
@@ -26,60 +34,60 @@ const Portfolio = () => {
 
   return (
     <div className="text-[#7A6960]">
-      <h1 className="text-3xl font-semibold text-center">Portfolio</h1>
-      <div className="flex justify-center items-center mt-2">
-        <div className="bg-orange-600 h-1 w-16 rounded"></div>
-      </div>
-      <p className="text-gray-700 text-center mt-7 mb-10 text-lg">
-        Explore Our Live Courses & Make Bharat ka Career Launchpad
+          <h1 className="heading-style">Portfolio</h1>
+          <div className="flex justify-center items-center mt-2 mb-10">
+              <div className="bg-blue-400 h-1 w-16 rounded"></div>
+          </div>
+          <p className="text-gray-700 text-center mt-7 mb-10 text-lg px-4">
+              We craft high-impact videos, stunning visuals, and engaging brand stories. Check out our latest work and see how we bring ideas to life!
       </p>
 
       {/* Images */}
       <div className="flex flex-col justify-center items-center lg:-gap-6 gap-3 p-5 mb-10">
         {/* 1st row */}
         <div data-aos="fade-right" className="flex flex-col md:flex-row lg:-gap-6 gap-3">
-          <div onClick={() => navigate("portfolio/app1")}>
-            <PortfolioProduct image={app1} />
+                  <div onClick={() => navigate("portfolio/app1")}>
+                      <PortfolioProduct image={photoShootIcon} title={'Photography'} />
           </div>
 
-          <div onClick={() => navigate("portfolio/product1")}>
-            <PortfolioProduct image={product1} />
+                  <div onClick={() => navigate("portfolio/product1")}>
+                      <PortfolioProduct image={SocialIcon} title={'Social Media Creatives'} />
           </div>
 
-          <div onClick={() => navigate("portfolio/brand1")}>
-            <PortfolioProduct image={branding1} />
+                  <div onClick={() => navigate("portfolio/brand1")}>
+                      <PortfolioProduct image={VideosIcon} title={'Videos'} />
           </div>
         </div>
 
         {/* 2nd row */}
-        <div data-aos="fade-left" className=" md:flex lg:-gap-6 gap-3 hidden">
-          <div onClick={() => navigate("portfolio/book1")}>
-            <PortfolioProduct image={book1} />
-          </div>
+        {/*<div data-aos="fade-left" className=" md:flex lg:-gap-6 gap-3 hidden">*/}
+        {/*  <div onClick={() => navigate("portfolio/book1")}>*/}
+        {/*    <PortfolioProduct image={book1} />*/}
+        {/*  </div>*/}
 
-          <div onClick={() => navigate("portfolio/app2")}>
-            <PortfolioProduct image={app2} />
-          </div>
+        {/*  <div onClick={() => navigate("portfolio/app2")}>*/}
+        {/*    <PortfolioProduct image={app2} />*/}
+        {/*  </div>*/}
 
-          <div onClick={() => navigate("portfolio/book3")}>
-            <PortfolioProduct image={book3} />
-          </div>
-        </div>
+        {/*  <div onClick={() => navigate("portfolio/book3")}>*/}
+        {/*    <PortfolioProduct image={book3} />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
 
         {/* 3rd row */}
-        <div data-aos="fade-up" className="lg:-gap-6 gap-3 hidden md:flex">
-        <div onClick={() => navigate("portfolio/product3")}>
-            <PortfolioProduct image={product3} />
-          </div>
+        {/*<div data-aos="fade-up" className="lg:-gap-6 gap-3 hidden md:flex">*/}
+        {/*<div onClick={() => navigate("portfolio/product3")}>*/}
+        {/*    <PortfolioProduct image={product3} />*/}
+        {/*  </div>*/}
 
-          <div onClick={() => navigate("portfolio/book2")}>
-            <PortfolioProduct image={book2} />
-          </div>
+        {/*  <div onClick={() => navigate("portfolio/book2")}>*/}
+        {/*    <PortfolioProduct image={book2} />*/}
+        {/*  </div>*/}
 
-          <div onClick={() => navigate("portfolio/app3")}>
-            <PortfolioProduct image={app3} />
-          </div>
-        </div>
+        {/*  <div onClick={() => navigate("portfolio/app3")}>*/}
+        {/*    <PortfolioProduct image={app3} />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </div>
   );
--- src/components/PortfolioProduct.jsx
+++ src/components/PortfolioProduct.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { FaLink } from "react-icons/fa6";
 import "../index.css";
+//import videoSrc from "../img/video/TEST.mov";
 
 
-const PortfolioProduct = ({image}) => {
+
+const PortfolioProduct = ({image,title}) => {
   return (
     <div className="w-[26rem] overflow-hidden hover:cursor-pointer hover:text-orange-600 myLink">
       <img
         src={image}
         className="w-fit h-fit object-cover transition-all duration-300 hover:opacity-50 hover:scale-110"
         alt="phone"
-      />
-      <div className="link">
-        <FaLink size={"45px"} />
-      </div>
+          />
+          {/*<video*/}
+          {/*    src={videoSrc}*/}
+          {/*    className="w-fit h-fit object-cover transition-all duration-300 hover:opacity-50 hover:scale-110"*/}
+          {/*    controls*/}
+          {/*    muted*/}
+          {/*    loop*/}
+          {/*    autoPlay*/}
+          {/*/>*/}
+      {/*<div className="link">*/}
+      {/*  <FaLink size={"45px"} />*/}
+      {/*    </div>*/}
+
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white font-bold text-xl opacity-0 transition-opacity duration-300 hover:opacity-100">
+              {title}
+          </div>
     </div>
   );
 };
