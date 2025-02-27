76fe241f963072d27546b2767e46a648be7c3f13
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-28 00:43:50 +0800

    no message

--- src/components/PortfolioProduct.jsx
+++ src/components/PortfolioProduct.jsx
@@ -7,27 +7,16 @@ import "../index.css";
 const PortfolioProduct = ({image,title}) => {
   return (
       <div className="w-full max-w-[380px] md:max-w-[420px] lg:max-w-[500px] overflow-hidden hover:cursor-pointer hover:text-orange-600 myLink">
-      <img
-        src={image}
-        className="w-fit h-fit object-cover transition-all duration-300 hover:opacity-50 hover:scale-110"
-        alt="phone"
+          <img
+              src={image}
+              className="w-full h-auto max-h-[300px] object-cover rounded-lg transition-all duration-300 hover:opacity-50 hover:scale-110"
+              alt={title}
           />
-          {/*<video*/}
-          {/*    src={videoSrc}*/}
-          {/*    className="w-fit h-fit object-cover transition-all duration-300 hover:opacity-50 hover:scale-110"*/}
-          {/*    controls*/}
-          {/*    muted*/}
-          {/*    loop*/}
-          {/*    autoPlay*/}
-          {/*/>*/}
-      {/*<div className="link">*/}
-      {/*  <FaLink size={"45px"} />*/}
-      {/*    </div>*/}
-
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white font-bold text-xl opacity-0 transition-opacity duration-300 hover:opacity-100">
               {title}
           </div>
-    </div>
+      </div>
+
   );
 };
 
