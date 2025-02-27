2eb5cff25734e39ef5028f16782a9b7c2d3afe38
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-02-28 00:46:58 +0800

    no message

--- src/components/PortfolioProduct.jsx
+++ src/components/PortfolioProduct.jsx
@@ -9,13 +9,15 @@ const PortfolioProduct = ({image,title}) => {
       <div className="w-full max-w-[380px] md:max-w-[420px] lg:max-w-[500px] overflow-hidden hover:cursor-pointer hover:text-orange-600 myLink">
           <img
               src={image}
-              className="w-full h-auto max-h-[300px] object-cover rounded-lg transition-all duration-300 hover:opacity-50 hover:scale-110"
+              className="w-full h-fit max-h-[300px] object-cover rounded-lg transition-all duration-300 hover:opacity-50 hover:scale-110"
               alt={title}
           />
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white font-bold text-xl opacity-0 transition-opacity duration-300 hover:opacity-100">
               {title}
           </div>
       </div>
+
+
 
   );
 };
