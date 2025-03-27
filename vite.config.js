bc99c5bfeca682202efa909178689a6e7e421c03
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2025-03-27 17:19:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './', // Change '/' to './' for Cloudflare Pages
+    base: '/', // Change '/' to './' for Cloudflare Pages
     plugins: [react()],
 });
 
